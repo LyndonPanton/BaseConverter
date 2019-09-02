14ecba81f50c122049aeae63c4c1505a734cb830
Lyndon Panton <LyndonPanton@gmail.com>
2019-09-02 21:17:44 +0100

    added logic to convert number to defined base

--- js/app.js
+++ js/app.js
@@ -5,5 +5,29 @@ window.onload = function(event) {
 	let form = document.getElementsByTagName("form")[0];
 	form.addEventListener("submit", function(event) {
 		event.preventDefault();
+
+		const result = convert(number, base);
 	});
+
+	function convert(number, base) {
+		let result = "";
+
+		while (number > 0) {
+			let bit;
+			let quotient;
+
+			if (number % base < 10) {
+				bit = number % bit;
+			} else {
+				bit = String.fromCharCode((number % base) + 55);
+			}
+
+			quotient = Math.floor(number / base);
+
+			result = bit + result;
+			number = quotient;
+		}
+
+		return result;
+	}
 };
