f8a7145e7e5e8142aedfae08f1e2b499bfc3fc04
Lyndon Panton <LyndonPanton@gmail.com>
2019-09-02 21:14:39 +0100

    prevented default page submission

--- js/app.js
+++ js/app.js
@@ -1,4 +1,9 @@
 window.onload = function(event) {
 	const copyrightYear = document.getElementById("copyright-year");
-	copyrightYear.textContent = (new Date()).getFullYear()
+	copyrightYear.textContent = (new Date()).getFullYear();
+
+	let form = document.getElementsByTagName("form")[0];
+	form.addEventListener("submit", function(event) {
+		event.preventDefault();
+	});
 };
